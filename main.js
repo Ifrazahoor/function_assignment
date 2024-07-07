9d0097fd5986d0d7d0d05064178a837751444778
Ifrazahoor <166019853+Ifrazahoor@users.noreply.github.com>
2024-07-07 15:34:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+function add(num1, num2) {
+    return num1 + num2;
+}
+let sum = add(99, 67);
+console.log(sum);
+function sub(num9, num8) {
+    return num9 - num8;
+}
+let min = sub(999, 1222);
+console.log(min);
+function multiplication(num4, num6) {
+    return num4 * num6;
+}
+let multi = multiplication(8766, 7855);
+console.log(multi);
+function divide(num5, num2) {
+    return num5 / num2;
+}
+let div = divide(777, 909);
+console.log(div);
---
+++ main.ts
@@ -0,0 +1,20 @@
+function add(num1:number,num2:number):number{
+    return num1+num2
+}
+let sum=add(99,67);
+console.log(sum)
+function sub(num9:number,num8:number):number{
+    return num9-num8
+}
+let min=sub(999,1222)
+console.log(min);
+function multiplication(num4:number,num6:number):number{
+    return num4*num6
+}
+let multi=multiplication(8766,7855)
+console.log(multi);
+function divide(num5:number,num2:number):number{
+    return num5/num2
+}
+let div=divide(777,909)
+console.log(div);
